96bee5c64066a3d7d778c6ef717946bf8743444b
BRIAN <owner@MBP-de-Brian.home>
2023-11-23 19:37:26 +0000

    att

---
+++ main.js
@@ -0,0 +1,76 @@
+// Exercise 1
+alert("Hello world!");
+
+// Exercise 2
+let x = 10;
+let y = 10;
+
+let sum = x + y;
+
+alert("A soma é:" + sum);
+
+// Exercise 3
+let numero = 1;
+
+if (typeof numero == "number") {
+  alert("É um número");
+} else {
+  alert("Não é um número");
+}
+
+// Exercise 4
+let string = "String";
+
+if (typeof string == "string") {
+  alert("É uma string");
+} else {
+  alert("Não é uma string");
+}
+
+// Exercise 5
+let boolean = true;
+
+if (typeof boolean == "boolean") {
+  alert("É um boolean");
+} else {
+  alert("Não é um boolean");
+}
+
+// Exercise 6
+const a = 5
+const b = 10
+
+const sub = b - a;
+alert("A subtração é: " + sub);
+
+// Exercise 7
+const c = 5
+const d = 10
+
+const mult = c * d;
+alert("A multiplicação é: " + mult);
+
+// Exercise 8
+const e = 5
+const f = 10
+
+const div = f / e;
+alert("A divisão é: " + div);
+
+// Exercise 9
+let par = 10;
+
+if (par % 2 == 0) {
+  alert("É um par");
+} else {
+  alert("Não é um par");
+}
+
+// Exercise 10
+let impar = 7;
+
+if (impar % 2 != 0) {
+  alert("É um impar");
+} else {
+  alert("Não é um impar");
+}
